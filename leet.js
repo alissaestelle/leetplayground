0d34900933f18c311c2a02b7b2043a695bc6c4cf
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-06-02 11:59:00 -0400

    #1929 Solved

--- leet.js
+++ leet.js
@@ -1,4 +1,5 @@
-// Time it Took to Solve (No Google/Hints): 1.5 Days
+// #1920
+// Solve Time (No Google/Hints): 1.5 Days
 const buildArray = (nums) => {
   let ans = nums.map((num, idx) => {
     return nums[nums[idx]]
@@ -6,5 +7,17 @@ const buildArray = (nums) => {
   return ans
 }
 
-let solution = buildArray([5, 0, 1, 2, 3, 4])
-console.log(solution)
+// let solution = buildArray([5, 0, 1, 2, 3, 4])
+
+// #1929 Solve Time (No Google/Hints): 1hr
+const getConcat = (nums) => {
+  let ans = [...nums]
+
+  nums.map((num) => {
+    ans.push(num)
+    return nums
+  })
+  return ans
+}
+
+let solution = getConcat([1, 3, 2, 1])
