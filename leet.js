677fef26c9342128aac8d1324f534e52f5d2fc65
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-06-03 15:45:36 -0400

    #1672 Solved

--- leet.js
+++ leet.js
@@ -8,16 +8,48 @@ const buildArray = (nums) => {
 }
 
 // let solution = buildArray([5, 0, 1, 2, 3, 4])
+// console.log(`Solution: ${solution}`)
 
 // #1929 Solve Time (No Google/Hints): 1hr
 const getConcat = (nums) => {
   let ans = [...nums]
 
   nums.map((num) => {
-    ans.push(num)
-    return nums
+    return ans.push(num)
   })
+
   return ans
 }
 
-let solution = getConcat([1, 3, 2, 1])
+// let solution = getConcat([1, 3, 2, 1])
+// console.log(`Solution: ${solution}`)
+
+// #1480 Solve Time (No Google/Hints): 30mins
+const runningSum = (nums) => {
+  let runSum = nums.map((num, idx) => {
+    return idx === 0 ? nums[idx] : (nums[idx] += nums[idx - 1])
+  })
+  return runSum
+}
+
+// let solution = runningSum([3, 1, 2, 10, 1])
+// console.log(`Solution: ${solution}`)
+
+// #1672 Solve Time (Mark Assist w/ for Loop): 4hrs
+const maxWealth = (accounts) => {
+  let max = 0
+  accounts.forEach((account) => {
+    let sum = account.reduce((acc, elem) => {
+      return acc + elem
+    }, 0)
+    if (sum > max) max = sum
+  })
+  return max
+}
+
+// let solution = maxWealth([
+//   [2, 8, 7],
+//   [7, 1, 3],
+//   [1, 9, 5]
+// ])
+// console.log(`Solution: ${solution}`)
